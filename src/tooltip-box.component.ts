a962bfd6d5310382999f910b32b034c48b0190da
Alione Thetaris <alione@thetaris.com>
2019-03-13 19:04:35 +0200

    add centered position

--- src/tooltip-box.component.ts
+++ src/tooltip-box.component.ts
@@ -25,12 +25,14 @@ import {
   styles: [
       `
           :host {
-              background-color: rgba(0, 0, 0, 0.8);
-              color: white;
+              background-color: white;
+              color: grey;
               display: inline-block;
+              border-radius: 10px;
               position: fixed;
-              padding: 15px 25px;
+              padding: 13px 15px;
               font-size: 15px;
+              box-shadow: 0 0 15px rgba(0,0,0,0.3);
           }
     `,
       `
@@ -42,10 +44,12 @@ import {
               height: 0;
           }
     `,
-    ':host.has-arrow.arrow-top:before { border-bottom: 5px solid rgba(0,0,0,0.8); top: -10px; }',
-    ':host.has-arrow.arrow-left:before { border-right: 5px solid rgba(0,0,0,0.8); left: -10px; }',
-    ':host.has-arrow.arrow-right:before { border-left: 5px solid rgba(0,0,0,0.8); right: -10px; }',
-    ':host.has-arrow.arrow-bottom:before { border-top: 5px solid rgba(0,0,0,0.8); bottom: -10px; }',
+    ':host.has-arrow.arrow-top:before { border-bottom: 5px solid white; top: -10px; }',
+    ':host.has-arrow.arrow-left:before { border-right: 5px solid white; left: -10px; }',
+    ':host.has-arrow.arrow-right:before { border-left: 5px solid white; right: -10px; }',
+    ':host.has-arrow.arrow-bottom:before { border-top: 5px solid white; bottom: -10px; }',
+    ':host.has-arrow.arrow-bottom-center:before { border-top: 5px solid white; bottom: -10px;left: 50%;transform: translateX(-50%); }',
+    ':host.has-arrow.arrow-top-center:before { border-bottom: 5px solid white; top: -10px;left: 50%;transform: translateX(-50%); }',
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
